a62a781ddef9d64701068f70d4c7aa2a5e4431f9
郝仕兢 <haoshijing690@163.com>
2018-05-12 20:09:22 +0800

    修改系统配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"prod"',
-    BASE_API: '"http://39.104.52.84:8887/huankeadmin/"',
+    BASE_API: '"http://www.hcocloud.com:8887/huankeadmin/"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
