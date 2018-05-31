ca4607c3ddffc0e5cd273edc6e756fb9db64a1b5
郝仕兢 <haoshijing690@163.com>
2018-05-31 23:38:14 +0800

    增加设备组

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,7 @@ export const asyncRouterMap = [
     },
     children: [
       { path: 'deviceList', component: _import('extend/device/index'), name: '设备列表', meta: { title: '设备列表' }},
+      { path: 'deviceGroup', component: _import('extend/device/group'), name: '设备组列表', meta: { title: '设备组列表' }},
       { path: 'deviceMap', component: _import('extend/device/deviceMap'), name: '设备地图展示', meta: { title: '设备地图展示' }}
     ]
   },
