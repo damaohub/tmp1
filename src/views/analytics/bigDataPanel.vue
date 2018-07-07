e2450286a44d098ddea7f6e8a16c6a63c04f3f27
ovo4096 <ovo4096@gmail.com>
2018-07-07 12:31:32 +0800

    feat(views): 添加大数据看板页

---
+++ src/views/analytics/bigDataPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="big-data-panel">大数据看板 <router-link to="/">返回</router-link></div>
+</template>
+
+<style lang="scss" scoped>
+  .big-data-panel {
+    top: 0;
+    position: fixed;
+    left: 0;
+    right: 0;
+    background-color: #ccc;
+    bottom: 0;
+  }
+</style>
