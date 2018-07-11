69c7104cefc7f7a11a59e3f10ec604fa1f2a2311
ovo4096 <ovo4096@gmail.com>
2018-07-12 03:37:53 +0800

    feat(views/login): 移植登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,52 +1,51 @@
 <template>
   <div class="login-container">
-
-    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">{{$t('login.title')}}</h3>
-        <lang-select class="set-language"></lang-select>
-      </div>
-
-      <el-form-item prop="username">
+    <el-card class="login-card">
+      <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
+        <div class="login-header">
+          <div class="login-header__logo">
+            <img src="/static/images/logo.svg" alt="logo">
+          </div>
+          <div class="login-header__title">空气管理平台</div>
+        </div>
+        <el-form-item prop="username">
         <span class="svg-container svg-container_login">
           <svg-icon icon-class="user" />
         </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" :placeholder="$t('login.username')"
-        />
-      </el-form-item>
+          <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" :placeholder="$t('login.username')"
+          />
+        </el-form-item>
 
-      <el-form-item prop="password">
+        <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password" />
         </span>
-        <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          :placeholder="$t('login.password')" />
-        <span class="show-pwd" @click="showPwd">
+          <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+                    :placeholder="$t('login.password')" />
+          <span class="show-pwd" @click="showPwd">
           <svg-icon icon-class="eye" />
         </span>
-      </el-form-item>
+        </el-form-item>
+        <div class="login-form-item">
+          <el-button type="text" @click="showDialog = true">忘记密码?</el-button>
+        </div>
+        <div class="login-form-item">
+          <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
+        </div>
+        <!--<el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{$t('login.thirdparty')}}</el-button>-->
+      </el-form>
+    </el-card>
 
-      <el-button type="primary" style="width:100%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
+    <el-dialog title="密码找回提示" :visible.sync="showDialog" append-to-body>
+      <pre>
+        *1 若超级管理员忘记密码，可以联系环可平台客服人员；
 
-      <div class="tips">
-        <span>{{$t('login.username')}} : admin</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div>
-      <div class="tips">
-        <span style="margin-right:18px;">{{$t('login.username')}} : editor</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div>
+        *2 若为其它角色忘记密码，可以联系该角色的上级管理员（即该子平台的超级管理员）；
 
-      <el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{$t('login.thirdparty')}}</el-button>
-    </el-form>
+        *3 点击忘记密码应弹出相应的密码找回页面，该页面有上述两个途径可以找回密码；
 
-    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
-      {{$t('login.thirdpartyTips')}}
-      <br/>
-      <br/>
-      <br/>
-      <social-sign />
+        （暂不支持线上找回功能，因本平台针对的是专业管理用户，非终端用户）
+      </pre>
     </el-dialog>
 
   </div>
@@ -63,14 +62,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!isvalidUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码不能小于 6 位'))
       } else {
         callback()
       }
@@ -112,31 +111,7 @@ export default {
           return false
         }
       })
-    },
-    afterQRScan() {
-      // const hash = window.location.hash.slice(1)
-      // const hashObj = getQueryObject(hash)
-      // const originUrl = window.location.origin
-      // history.replaceState({}, '', originUrl)
-      // const codeMap = {
-      //   wechat: 'code',
-      //   tencent: 'code'
-      // }
-      // const codeName = hashObj[codeMap[this.auth_type]]
-      // if (!codeName) {
-      //   alert('第三方登录失败')
-      // } else {
-      //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-      //     this.$router.push({ path: '/' })
-      //   })
-      // }
     }
-  },
-  created() {
-    // window.addEventListener('hashchange', this.afterQRScan)
-  },
-  destroyed() {
-    // window.removeEventListener('hashchange', this.afterQRScan)
   }
 }
 </script>
@@ -175,7 +150,7 @@ $light_gray:#eee;
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#2d3a4b;
+$bg:#3b516d;
 $dark_gray:#889aa4;
 $light_gray:#eee;
 
@@ -184,14 +159,35 @@ $light_gray:#eee;
   height: 100%;
   width: 100%;
   background-color: $bg;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   .login-form {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 520px;
-    padding: 35px 35px 15px 35px;
-    margin: 120px auto;
   }
+
+  .login-form-item {
+    margin-bottom: 22px;
+  }
+
+  .login-header {
+    text-align: center;
+    &__logo {
+      margin-bottom: 22px;
+    }
+    &__title {
+      color: #eee;
+      font-size: 22px;
+      margin-bottom: 22px;
+    }
+  }
+
+  .login-card {
+    background-color: #2d3a4b;
+    border: none;
+    min-width: 400px;
+  }
+
   .tips {
     font-size: 14px;
     color: #fff;
